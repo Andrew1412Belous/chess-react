c8ce80f5f9b802ea2c2f0bd0103a3f96e77112e3
Andrii <andrew.belous1412@gmail.com>
2023-07-28 11:52:36 +0300

    add coordinates, configure board

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -6,7 +6,11 @@ interface CellProps {
 }
 
 const CellComponent: React.FC<CellProps> = ({ cell }) => {
-	return <div className={['cell', cell.color].join(' ')}>{cell.figure}</div>;
+	return (
+		<div className={['cell', cell.color].join(' ')}>
+			{cell.figure?.logo && <img src={cell.figure.logo} />}
+		</div>
+	);
 };
 
 export default CellComponent;
